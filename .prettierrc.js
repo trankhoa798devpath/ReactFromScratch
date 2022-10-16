3c36a9bb095c3a904ccf8d8506607b3fac9c3e02
trankhoa <trankhoa798@gmal.com>
2022-10-16 12:29:35 +0700

    feat: add prettier rules [US001]

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    useTabs: false,
+    printWidth: 120,
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'none',
+    importOrder: ['<THIRD_PARTY_MODULES>', '^[./]'],
+    importOrderSeparation: true,
+    importOrderGroupNamespaceSpecifiers: true
+};
